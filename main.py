558579465a1731a4887ee149233f4e622d5e9e39
Isaac López <isaaklop2003@gmail.com>
2023-03-14 09:46:10 -0600

    programa mayor actualizado
    Creacion del programa del primer inciso

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+mayor = 0
+residuo = 0
 n1 = int(input("ingrese el primer numero"))
-n1 = int(input("ingrese el segundo numero"))
+n2 = int(input("ingrese el segudo numero"))
 if n1 > mayor:
     mayor = n1
 elif n2 > mayor:
     mayor = n2
-print("el numero mayor es:", mayor)
+print("el numero mayor es:",mayor)
