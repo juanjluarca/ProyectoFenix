e6dd028f36be1303c55b829073328bebd69f0e4b
Isaac López <isaaklop2003@gmail.com>
2023-03-14 09:41:31 -0600

    Programa mayor creado
    Se creó el programa para determinar el mayor de dos numeros

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-mayor = 0
-residuo = 0
+n1 = int(input("ingrese el primer numero"))
+n1 = int(input("ingrese el segundo numero"))
+if n1 > mayor:
+    mayor = n1
+elif n2 > mayor:
+    mayor = n2
+print("el numero mayor es:", mayor)
